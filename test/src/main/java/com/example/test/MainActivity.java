18b8b768718bf2b7c19761615666df593621a3ba
坚果 <3772548978@qq.com>
2026-03-20 18:17:36 +0800

    refactor: 移除全屏功能及相关样式配置
    删除AndroidManifest.xml中的主题引用
    移除styles.xml文件及所有样式定义
    简化MainActivity，删除全屏功能代码

--- test/src/main/java/com/example/test/MainActivity.java
+++ test/src/main/java/com/example/test/MainActivity.java
@@ -2,55 +2,14 @@ package com.example.test;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Build;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         TextView textView = new TextView(this);
         textView.setText("Hello, Android!");
-        textView.setTextSize(24);
-        textView.setGravity(android.view.Gravity.CENTER);
         setContentView(textView);
-        
-        enableFullScreen();
-    }
-    
-    private void enableFullScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            getWindow().setDecorFitsSystemWindows(false);
-            WindowInsetsController controller = getWindow().getInsetsController();
-            if (controller != null) {
-                controller.hide(WindowInsets.Type.systemBars());
-                controller.setSystemBarsBehavior(
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                );
-            }
-        } else {
-            getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            );
-            getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            );
-        }
-    }
-    
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            enableFullScreen();
-        }
     }
 }
